c27fe56cd3389b3457bec5027daa7a8892d2be9b
Charlie <charlie.chuangqi.li@gmail.com>
2023-01-19 14:31:35 +1000

    build header style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 function App() {
   return (
     <>
       <Header></Header>
+      <Home></Home>
     </>
   );
 }
