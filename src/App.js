1e0f96fbfc06c680410a2bcb337bf382c4c1a03d
Charlie <charlie.chuangqi.li@gmail.com>
2023-01-19 21:21:39 +1000

    home part setup and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 function App() {
   return (
     <>
-      <Header></Header>
-      <Home></Home>
+      <Header />
+      <main className='main'>
+        <Home />
+        <About />
+      </main>
     </>
   );
 }
