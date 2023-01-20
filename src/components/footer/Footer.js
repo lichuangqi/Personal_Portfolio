43a0441dfa094ad12eedc87f781d105e44529536
Charlie <charlie.chuangqi.li@gmail.com>
2023-01-21 01:21:25 +1000

    footer page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,23 @@
 import './footer.css';
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className="footer__container container">
+        <h1 className="footer__title">Charlie</h1>
+        <ul className='footer__list'>
+          <li><a href='#about' className='footer__link'>About</a></li>
+          <li><a href='#portfolio' className='footer__link'>Projects</a></li>
+          <li><a href='#testimonials' className='footer__link'>Testimonials</a></li>
+        </ul>
+        <div className="footer__social">
+          <a href="https://www.facebook.com/" className="footer__social-link" target="_blank"><i className="bx bxl-facebook"></i></a>
+          <a href="https://www.instagram.com/" className="footer__social-link" target="_blank"><i className="bx bxl-instagram"></i></a>
+          <a href="https://twitter.com/" className="footer__social-link" target="_blank"><i className="bx bxl-twitter"></i></a>
+        </div>
+        <span className='footer__copy'>&#169; CharlieLee. All rigths reserved
+        </span>
+      </div>
+    </footer>
   )
 }
 
