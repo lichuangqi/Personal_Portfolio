fb3d7b73f10cb92c3c2febbb0ce77d524d69ef50
Charlie <charlie.chuangqi.li@gmail.com>
2023-01-24 00:31:55 +1000

    work page update

--- src/components/work/Data.js
+++ src/components/work/Data.js
@@ -6,25 +6,29 @@ export const projectsData = [
   {
     id: 1,
     image: Cover_1,
-    title: "Land Monitor app",
+    title: "Land Data Monitor app",
+    url:"https://www.vizen.cn/#/business/dataService",
     category: "app",
   },
   {
     id: 2,
     image: Cover_2,
     title: "Smart City website",
+    url:"https://show.vizen.cn/vbrFactory",
     category: "Web",
   },
   {
     id: 3,
     image: Cover_3,
     title: "Medical Data big screen",
+    url:"http://www.medextech.com/newsinfo/4938411.html",
     category: "Design",
   },
   {
     id: 4,
     image: Cover_4,
     title: "JR Academy website",
+    url:"https://jracademy.com.au/",
     category: "web",
   }
 ];
